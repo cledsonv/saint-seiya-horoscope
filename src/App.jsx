938fedfb2c411d04749284fcdc05ca115ffcd68c
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2024-04-28 15:45:18 -0300

    refactor background

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ZodiacGrid from './components/ZodiacGrid';
 import HoroscopePage from './components/HoroscopePage';
 import './App.css';
+import HomePage from './pages/HomePage';
+import NavBar from './components/NavBar';
+import About from './pages/AboutPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <Routes>
-          <Route path="/" element={<ZodiacGrid />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
           <Route path="/horoscope/:sign" element={<HoroscopePage />} />
         </Routes>
       </div>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/NavBar.css';
+import logo from '../assets/logo.webp';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink to="/" className="nav-logo" > <img src={logo} alt="Logo" /></NavLink>
+      <ul className="nav-menu">
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link" end>Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/about" className="nav-link">About</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/services" className="nav-link">Services</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/contact" className="nav-link">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
