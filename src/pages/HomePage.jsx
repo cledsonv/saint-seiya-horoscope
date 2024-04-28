e8c3d3cb4e6abf6e4511d2955fa6e88070c8c002
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2024-04-28 15:44:48 -0300

    refactor homePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import ZodiacGrid from "../components/ZodiacGrid";
+import '../styles/HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="Home">
+        <ZodiacGrid></ZodiacGrid>
+      </div>
+  );
+}
+
+export default HomePage
